30500a10230de824b08035628b22d5f896883512
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-05-10 00:04:33 +0300

    добавил новые поля у категории

--- src/models/category/index.ts
+++ src/models/category/index.ts
@@ -6,12 +6,20 @@ import type { ICategorySchema, TCategoryQueries } from './types';
 
 const CategorySchema = new Schema<ICategorySchema, {}, {}, TCategoryQueries>({
 	name: { type: String, required: true, trim: true },
-	subCategories: { type: [Schema.Types.ObjectId], ref: 'Category' },
+	photo: { type: String, required: false},
+	parentId: { type: Schema.Types.ObjectId, ref: 'Category', default: null},
 	products: { type: [Schema.Types.ObjectId], ref: Product, required: true },
+	featured: {type: Boolean, required: false},
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: null },
+	deletedAt: { type: Date, default: null },
 });
 
-CategorySchema.query.getProducts = async function () {
+CategorySchema.query.getCategories = async function () {
 	return this.products;
 };
+
+// авто-удаление из бд через месяц после того как появилось поле deletedAt с временем удаления
+CategorySchema.index({ "deletedAt": 1 }, { expireAfterSeconds: 60 });
 
 export const Category = model<ICategorySchema>('Category', CategorySchema);
