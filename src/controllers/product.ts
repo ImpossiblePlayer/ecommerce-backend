5f5ad7324ae24906ac50b676c580d2fae23d52d6
Kirill <pavlovkirill322@gmail.com>
2023-06-14 23:20:17 +0300

    создание продукта

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import * as process from 'process';
+
+import fileUpload from 'express-fileupload';
+import mongoose from 'mongoose';
+
 import { Product } from '@models';
-import { OK_200, NotFound_404, InternalError_500 } from '@services';
+import {
+  OK_200,
+  NotFound_404,
+  InternalError_500,
+  BadRequest_400,
+  convertImagesFlat,
+} from '@services';
 
 import type { Request, Response } from 'express';
 
@@ -22,7 +34,56 @@ export const GetProduct = async (req: Request, res: Response) => {
 
 export const CreateProduct = async (req: Request, res: Response) => {
   try {
-    // const {};
+    const {
+      name,
+      category,
+      description,
+      deliveryCost,
+      quantity,
+      advantages,
+      price,
+      productId,
+      mainPhoto,
+      additionalPhotos,
+    } = req.body;
+
+    console.log(productId, mainPhoto, additionalPhotos);
+    if (
+      !name ||
+      !description ||
+      !quantity ||
+      !price ||
+      !advantages ||
+      !category ||
+      !deliveryCost
+    ) {
+      return BadRequest_400(res, {
+        message: 'data is not provided',
+      });
+    }
+    console.log(req.body);
+
+    // await Product.create({
+    //   _id: productId,
+    //   name: name,
+    //   description: description,
+    //   category: category,
+    //   quantity: quantity,
+    //   mainPhoto: mainPhoto,
+    //   additionalPhotos: additionalPhotos,
+    //   price: {
+    //     current: price.current,
+    //     old: price.old,
+    //   },
+    //   rating: 0,
+    //   soldQuantity: 0,
+    //   orders: 0,
+    //   advantages: advantages,
+    //   reviewsCount: 0,
+    //   sellerId: 'Test seller',
+    // });
+
+    return OK_200(res, { message: 'success' });
   } catch (err) {
     return InternalError_500(res);
   }
